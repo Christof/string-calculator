90ba364836b3efe5d0fea8467dfbb26b137b1e04
christof <christof.sirk@gmail.com>
2019-02-20 22:01:51 +0100

    Extract static parseSeparators method.

--- string_calculator.ts
+++ string_calculator.ts
@@ -1,15 +1,31 @@
+interface ParseSeparatorsResult {
+  separatorsRegex: RegExp;
+  remainingInput: string;
+}
 export class StringCalculator {
+  static parseSeparators(input: string): ParseSeparatorsResult {
+    if (input.startsWith('//')) {
+      const startOfContent = input.indexOf('\n');
+      return {
+        separatorsRegex: new RegExp(input.slice(2, startOfContent)),
+        remainingInput: input.slice(startOfContent + 1)
+      };
+    }
+
+    return {
+      separatorsRegex: /,|\n/,
+      remainingInput: input
+    };
+  }
+
   static add(input: string): number {
     if (input.length === 0) return 0;
 
-    let separatorRegex = /,|\n/;
-    if (input.startsWith('//')) {
-      const startOfContent = input.indexOf('\n');
-      separatorRegex = new RegExp(input.slice(2, startOfContent));
-      input = input.slice(startOfContent + 1);
-    }
+    const separatorsParseResult = StringCalculator.parseSeparators(input);
 
-    const numbersAsStrings = input.split(separatorRegex);
+    const numbersAsStrings = separatorsParseResult.remainingInput.split(
+      separatorsParseResult.separatorsRegex
+    );
     const numbers = numbersAsStrings.map(s => parseInt(s, 10));
 
     return numbers.reduce((accumulator, number) => accumulator + number, 0);
