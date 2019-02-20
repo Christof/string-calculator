1196a517d3bb3d76e4e46a4969be54ed31370413
christof <christof.sirk@gmail.com>
2019-02-20 21:50:27 +0100

    Calculate sum of multiple numbers.

--- string_calculator.test.ts
+++ string_calculator.test.ts
@@ -10,5 +10,9 @@ describe(`StringCalculator`, () => {
     it(`returns a given number, e.g. '1' results in 1`, () => {
       expect(StringCalculator.add('1')).to.equal(1);
     });
+
+    it(`returns sum of two given integers, e.g. '1,2' returns 3`, () => {
+      expect(StringCalculator.add('1,2')).to.equal(3);
+    });
   });
 });
--- string_calculator.ts
+++ string_calculator.ts
@@ -2,6 +2,9 @@ export class StringCalculator {
   static add(input: string): number {
     if (input.length === 0) return 0;
 
-    return parseInt(input);
+    const numbersAsStrings = input.split(',');
+    const numbers = numbersAsStrings.map(s => parseInt(s, 10));
+
+    return numbers.reduce((accumulator, number) => accumulator + number, 0);
   }
 }
