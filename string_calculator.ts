74c2d2345a5cd2e14cb8f99e1cc8d30bfb44d011
christof <christof.sirk@gmail.com>
2019-02-20 22:17:04 +0100

    Move most important function up.

--- string_calculator.ts
+++ string_calculator.ts
@@ -1,10 +1,26 @@
 import { escapeRegExp } from 'lodash';
 
-interface ParseSeparatorsResult {
-  separatorsRegex: RegExp;
-  remainingInput: string;
-}
 export namespace StringCalculator {
+  export function add(input: string): number {
+    if (input.length === 0) return 0;
+
+    const separatorsParseResult = parseSeparators(input);
+
+    const numbersAsStrings = separatorsParseResult.remainingInput.split(
+      separatorsParseResult.separatorsRegex
+    );
+    const numbers = numbersAsStrings.map(s => parseInt(s, 10));
+
+    throwErrorForNegativeNumbers(numbers);
+
+    return numbers.reduce((accumulator, number) => accumulator + number, 0);
+  }
+
+  interface ParseSeparatorsResult {
+    separatorsRegex: RegExp;
+    remainingInput: string;
+  }
+
   function parseSeparators(input: string): ParseSeparatorsResult {
     const defaultSeparators = ',|\n';
     if (input.startsWith('//')) {
@@ -22,21 +38,6 @@ export namespace StringCalculator {
       separatorsRegex: new RegExp(defaultSeparators),
       remainingInput: input
     };
-  }
-
-  export function add(input: string): number {
-    if (input.length === 0) return 0;
-
-    const separatorsParseResult = parseSeparators(input);
-
-    const numbersAsStrings = separatorsParseResult.remainingInput.split(
-      separatorsParseResult.separatorsRegex
-    );
-    const numbers = numbersAsStrings.map(s => parseInt(s, 10));
-
-    throwErrorForNegativeNumbers(numbers);
-
-    return numbers.reduce((accumulator, number) => accumulator + number, 0);
   }
 
   function throwErrorForNegativeNumbers(numbers: number[]) {
