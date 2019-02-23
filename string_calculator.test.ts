a0e08e5b462e6de2f14f0b4df6b81525064b39ba
christof <christof.sirk@gmail.com>
2019-02-23 21:30:30 +0100

    Add feature to parse a given number.

--- string_calculator.test.ts
+++ string_calculator.test.ts
@@ -6,5 +6,9 @@ describe(`StringCalculator`, () => {
     it(`returns 0 for empty string`, () => {
       expect(add('')).to.equal(0);
     });
+
+    it(`converts a given string into a number, e.g. '1' to 1`, () => {
+      expect(add('1')).to.equal(1);
+    });
   });
 });
