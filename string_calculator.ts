7c1233d312cab8444d23785e592c76fb750465ec
christof <christof.sirk@gmail.com>
2019-02-24 18:04:02 +0100

    Extract parseSeparators function.

--- string_calculator.ts
+++ string_calculator.ts
@@ -3,20 +3,27 @@ import { sum, escapeRegExp } from 'lodash';
 export function add(input: string) {
   if (input.length === 0) return 0;
 
+  const { separators, remainingInput } = parseSeparators(input);
+
+  return sum(
+    parseNumbers(
+      remainingInput,
+      new RegExp(separators.map(escapeRegExp).join('|'))
+    )
+  );
+}
+
+function parseSeparators(
+  input: string
+): { separators: string[]; remainingInput: string } {
   if (input.startsWith('//')) {
     const endOfCustomSeparator = input.indexOf('\n');
     const customSeparator = input.slice(2, endOfCustomSeparator);
     const remainingInput = input.slice(endOfCustomSeparator + 1);
-
-    return sum(
-      parseNumbers(
-        remainingInput,
-        new RegExp(',|\n|' + escapeRegExp(customSeparator))
-      )
-    );
+    return { separators: [',', '\n', customSeparator], remainingInput };
   }
 
-  return sum(parseNumbers(input, /,|\n/));
+  return { separators: [',', '\n'], remainingInput: input };
 }
 
 function parseNumbers(input: string, separatorRegex: RegExp): number[] {
