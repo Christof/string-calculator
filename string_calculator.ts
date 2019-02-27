112b490ee6a1bdbd99ace1474be3129b1a37a8a7
christof <christof.sirk@gmail.com>
2019-02-27 19:47:26 +0100

    Extract createSeparatorsRegex function.

--- string_calculator.ts
+++ string_calculator.ts
@@ -3,13 +3,14 @@ import { escapeRegExp } from 'lodash';
 export function add(input: string): number {
   if (input.length === 0) return 0;
 
+  const defaultSeparators = [',', '\n'];
   if (input.startsWith('//')) {
     const endOfCustomSeparator = input.indexOf('\n');
     const customSeparator = input.slice(2, endOfCustomSeparator);
     const remainingInput = input.slice(endOfCustomSeparator + 1);
 
     const numbersAsString = remainingInput.split(
-      new RegExp(`,|\n|${escapeRegExp(customSeparator)}`)
+      createSeparatorsRegex([...defaultSeparators, customSeparator])
     );
     const numbers = numbersAsString.map(numberAsString =>
       parseInt(numberAsString, 10)
@@ -17,9 +18,13 @@ export function add(input: string): number {
     return numbers.reduce((accumulator, number) => accumulator + number);
   }
 
-  const numbersAsString = input.split(/,|\n/);
+  const numbersAsString = input.split(createSeparatorsRegex(defaultSeparators));
   const numbers = numbersAsString.map(numberAsString =>
     parseInt(numberAsString, 10)
   );
   return numbers.reduce((accumulator, number) => accumulator + number);
+}
+
+function createSeparatorsRegex(separators: string[]): RegExp {
+  return new RegExp(separators.map(escapeRegExp).join('|'));
 }
