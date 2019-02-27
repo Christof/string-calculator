f6aaba8af4b3d3eeaf2d5363cc8d2e797cbe7c86
christof <christof.sirk@gmail.com>
2019-02-27 19:51:43 +0100

    Extract parseSeparator function.

--- string_calculator.ts
+++ string_calculator.ts
@@ -3,26 +3,34 @@ import { escapeRegExp, sum } from 'lodash';
 export function add(input: string): number {
   if (input.length === 0) return 0;
 
+  const { remainingInput, separators } = parseSeparator(input);
+
+  const numbersAsString = remainingInput.split(
+    createSeparatorsRegex(separators)
+  );
+  const numbers = numbersAsString.map(numberAsString =>
+    parseInt(numberAsString, 10)
+  );
+  return sum(numbers);
+}
+
+function parseSeparator(
+  input: string
+): { separators: string[]; remainingInput: string } {
   const defaultSeparators = [',', '\n'];
+
   if (input.startsWith('//')) {
     const endOfCustomSeparator = input.indexOf('\n');
     const customSeparator = input.slice(2, endOfCustomSeparator);
     const remainingInput = input.slice(endOfCustomSeparator + 1);
 
-    const numbersAsString = remainingInput.split(
-      createSeparatorsRegex([...defaultSeparators, customSeparator])
-    );
-    const numbers = numbersAsString.map(numberAsString =>
-      parseInt(numberAsString, 10)
-    );
-    return sum(numbers);
+    return {
+      separators: [...defaultSeparators, customSeparator],
+      remainingInput
+    };
   }
 
-  const numbersAsString = input.split(createSeparatorsRegex(defaultSeparators));
-  const numbers = numbersAsString.map(numberAsString =>
-    parseInt(numberAsString, 10)
-  );
-  return sum(numbers);
+  return { separators: defaultSeparators, remainingInput: input };
 }
 
 function createSeparatorsRegex(separators: string[]): RegExp {
