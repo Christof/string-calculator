5112a7dc5d0563a0d0b3c0b238b70afc2d1952f6
christof <christof.sirk@gmail.com>
2019-03-02 21:52:25 +0100

    Extract parseSeparators function.

--- string_calculator.ts
+++ string_calculator.ts
@@ -3,24 +3,30 @@ import { sum, escapeRegExp } from 'lodash';
 export function add(input: string): number {
   if (input.length === 0) return 0;
 
+  const { separators, remainingInput } = parseSeparators(input);
+  const numbersAsStrings = remainingInput.split(
+    createSeparatorsRegExp(separators)
+  );
+  const numbers = parseNumbers(numbersAsStrings);
+  return sum(numbers);
+}
+
+function parseSeparators(
+  input: string
+): { separators: string[]; remainingInput: string } {
   const defaultSeparators = [',', '\n'];
   if (input.startsWith('//')) {
     const endOfSeparator = input.indexOf('\n');
     const customSeparator = input.slice(2, endOfSeparator);
     const remainingInput = input.slice(endOfSeparator + 1);
 
-    const numbersAsStrings = remainingInput.split(
-      createSeparatorsRegExp([...defaultSeparators, customSeparator])
-    );
-    const numbers = parseNumbers(numbersAsStrings);
-    return sum(numbers);
+    return {
+      separators: [...defaultSeparators, customSeparator],
+      remainingInput
+    };
   }
 
-  const numbersAsStrings = input.split(
-    createSeparatorsRegExp(defaultSeparators)
-  );
-  const numbers = parseNumbers(numbersAsStrings);
-  return sum(numbers);
+  return { separators: defaultSeparators, remainingInput: input };
 }
 
 function parseNumbers(numbersAsStrings: string[]): number[] {
