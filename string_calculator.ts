6a182419540b585c4b140c8c1d839c121eab8c17
christof <christof.sirk@gmail.com>
2019-03-02 21:48:27 +0100

    Extract createSeparatorsRegExp function.

--- string_calculator.ts
+++ string_calculator.ts
@@ -3,23 +3,30 @@ import { sum } from 'lodash';
 export function add(input: string): number {
   if (input.length === 0) return 0;
 
+  const defaultSeparators = [',', '\n'];
   if (input.startsWith('//')) {
     const endOfSeparator = input.indexOf('\n');
     const customSeparator = input.slice(2, endOfSeparator);
     const remainingInput = input.slice(endOfSeparator + 1);
 
     const numbersAsStrings = remainingInput.split(
-      new RegExp(`,|\n|${customSeparator}`)
+      createSeparatorsRegExp([...defaultSeparators, customSeparator])
     );
     const numbers = parseNumbers(numbersAsStrings);
     return sum(numbers);
   }
 
-  const numbersAsStrings = input.split(/,|\n/);
+  const numbersAsStrings = input.split(
+    createSeparatorsRegExp(defaultSeparators)
+  );
   const numbers = parseNumbers(numbersAsStrings);
   return sum(numbers);
 }
 
 function parseNumbers(numbersAsStrings: string[]): number[] {
   return numbersAsStrings.map(numberAsString => parseInt(numberAsString, 10));
+}
+
+function createSeparatorsRegExp(separators: string[]) {
+  return new RegExp(separators.join('|'));
 }
